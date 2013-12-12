d595dfc56896851542e175a6488c24dbee8185a1
happyingly <longyily@126.com>
2013-12-13 02:46:34 +0800

    [add]UserDefaultHelper 增删改查UserDefault专用

---
+++ up/Application/Common/UPUserDefaultHelper.h
@@ -0,0 +1,13 @@
+//
+//  UPUserDefaultHelper.h
+//  up
+//
+//  Created by joy.long on 13-12-13.
+//  Copyright (c) 2013年 me.v2up. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UPUserDefaultHelper : NSObject
+
+@end
---
+++ up/Application/Common/UPUserDefaultHelper.m
@@ -0,0 +1,16 @@
+//
+//  UPUserDefaultHelper.m
+//  up
+//
+//  Created by joy.long on 13-12-13.
+//  Copyright (c) 2013年 me.v2up. All rights reserved.
+//
+
+#import "UPUserDefaultHelper.h"
+
+@implementation UPUserDefaultHelper
+
++ (void)setUserAvater:(NSString *)avater {
+    
+}
+@end
